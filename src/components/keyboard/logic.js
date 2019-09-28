08e7c745322b0c0a3165a8d1d84854b722442bb8
lfj <liao_fengji@163.com>
2019-09-28 09:40:26 +0800

    实现自定义数字键盘的逻辑

--- src/components/keyboard/logic.js
+++ src/components/keyboard/logic.js
@@ -41,22 +41,84 @@ export default {
         value: 11,
         label: '.'
       }],
-      keyword:''
+      keyword: ''
     }
   },
   /**
    * value  表示通过自定义键盘输入的值
    */
-  props:['value','end','pattern','type'],
+  props: ['value', 'end', 'pattern', 'type'],
   methods: {
     choosekey(item) {
-      console.log('item----->',item);
+      console.log('choosekey--------1111------>', item);
+      if (item.value == 12) {
+        this.$emit('closekeyboard', this.num);
+      } else {
+        if (this.type) {
+          if (item.value != 10) {
+            this.num.push(item.label);
+            if (!this.checkNum()) {
+              let arr = this.num;
+              let len = arr.length;
+              arr.splice(len - 1, 1);
+              this.num = arr;
+              return;
+            }
+            this.$emit('changekey', this.num);
+          }
+        } else {
+          if (this.num.length < 6) {
+            this.num.push(item.label);
+          }
+          //   console.log('this.num--->',this.num)
+          if (!this.checkNum()) {
+            let arr = this.num;
+            let len = arr.length;
+            arr.splice(len - 1, 1);
+            this.num = arr;
+            return;
+          }
+          this.$emit('changekey', this.num);
+        }
+      }
+    },
+    checkNum() {
+      //正常数字校验
+      let str = /^(\-)?\d+(\.\d{1,3})?$/;
+      let number = (this.num.join('')) + '0';
+      let status = true;
+      status = str.test(number);
+      console.log('checkNume', status);
+      return status;
+    },
+    init() {
+      if (this.value) {
+        let value = this.value.toString();
+        this.num = value.split('');
+      } else {
+        this.num = [];
+      }
+      this.isEnd = this.end;
+      this.cssStyle = this.pattern;
     },
     rebacking() {
-
+      //截取数组最后一位
+      if (this.num.length > 0) {
+        let num = this.num.filter(function (ele, idx, arr) {
+          return arr.length - 1 !== idx;
+        });
+        this.num = num;
+      } else {
+        this.num = [];
+      }
+      this.$emit('changekey', this.num);
+    },
+    nexting() {
+      this.$emit('nexting', this.isEnd);
     },
     makesure() {
-
+      event.stopPropagation();
+      this.$emit('makesure', this.num);
     }
   }
 }
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,48 +1,59 @@
 <template>
-    <div class="home">
-       <div class="search">
-            <Search
-            :placeholder="placeholder"
-            @height="getsearchheight"
-            @clearText="clearText"
-            @submit="submit"
-        />
-       </div>
-       <div>{{keyword}}</div>
-        <keyBoard/>
+  <div class="home">
+    <div class="search">
+      <Search
+        :placeholder="placeholder"
+        @height="getsearchheight"
+        @clearText="clearText"
+        @submit="submit"
+      />
     </div>
+    <div>{{keyword}}</div>
+    <keyBoard @closekeyboard="closekeyboard" @changekey="changekey" @checkNume="checkNume" />
+  </div>
 </template>
 <script>
-import keyBoard from '../components/keyboard';
-import Search from '../components/search';
+import keyBoard from "../components/keyboard";
+import Search from "../components/search";
 export default {
-    data(){
-        return {
-            heighting:{
-                search:0
-            },
-            placeholder:'输入关键字进行搜索',
-            keyword:''  // 自定义键盘输入的值
-        }
+  data() {
+    return {
+      heighting: {
+        search: 0
+      },
+      placeholder: "输入关键字进行搜索",
+      keyword: "" // 自定义键盘输入的值
+    };
+  },
+  components: {
+    keyBoard,
+    Search
+  },
+  methods: {
+    getsearchheight(value) {
+      console.log("搜索框的高度---->", value);
+      this.heighting["search"] = value;
     },
-    components:{
-        keyBoard,
-        Search
+    clearText() {
+      console.log("263764724679");
     },
-    methods:{
-        getsearchheight(value){
-            console.log('搜索框的高度---->',value);
-            this.heighting['search'] = value;
-        },
-        clearText(){
-            console.log('263764724679')
-        },
-        submit(value){
-            console.log('要搜索的内容---->',value);
-        }
+    submit(value) {
+      console.log("要搜索的内容---->", value);
+    },
+    // 
+    closekeyboard(value) {
+      console.log("value--closekeyboard-->", value);
+    },
+    changekey(value) {
+      console.log("value-changekey--->", value);
+    },
+    // 
+    checkNume(value) {
+      console.log("value-checkNume--->", value);
     }
-}
+  }
+};
 </script>
 <style lang="less" scoped>
-@import '../common/style/index.css';
+@import "../common/style/index.css";
 </style>
